3b1098799ed2639c6e77e6de7914b5b16d3a5774
haykodarb <haykodarb@gmail.com>
2021-08-21 22:53:08 -0300

    Added "loading" message

--- web/components/Slider/slider.js
+++ web/components/Slider/slider.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import styles from "./slider.module.css";
 
 export default function SliderShow({ uploaded }) {
-	const [images, setImages] = useState([]);
+	const [images, setImages] = useState(null);
 
 	const getTitle = () => {
 		return uploaded ? "Imagenes subidas" : "Imagenes no subidas";
@@ -26,14 +26,22 @@ export default function SliderShow({ uploaded }) {
 				{getTitle()}
 			</h1>
 			<div className={styles.container} draggable="false">
-				{images != null
-					? images.map((el) => {
-							let src = `data:${el.fileType};base64, ${el.b64content}`;
-							return (
-								<img src={src} className={styles.image} draggable="false" />
-							);
-					  })
-					: null}
+				{images != null ? (
+					images.map((el) => {
+						let src = `data:${el.fileType};base64, ${el.b64content}`;
+						return <img src={src} className={styles.image} draggable="false" />;
+					})
+				) : (
+					<p
+						style={{
+							flex: 1,
+							fontSize: "30px",
+							textAlign: "center",
+						}}
+					>
+						Cargando gatitos...
+					</p>
+				)}
 			</div>
 		</>
 	);
